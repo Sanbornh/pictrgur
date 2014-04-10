b4f5f30ac0e8f29dff118ac0da5efa95892ea9fd
Sanborn <sanbornh@rogers.com>
2014-04-10 14:54:50 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pictrgur::Application.config.secret_key_base = 'b2ceb9a40b92fab35e62af5464e6e92f998a5236edfece5f420fa75b75f1217a837c93f8082fdda53251b110521cd1210abf62ba9f67671c50ec7c0490348c4c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pictrgur::Application.config.session_store :cookie_store, key: '_pictrgur_session'

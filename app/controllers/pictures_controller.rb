e3e6292e0a594e298dd6db1cf840ef04186f0425
Sanborn <sanbornh@rogers.com>
2014-04-14 15:01:05 -0400

    Finished edit, create actions and added scoping to Picture class

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,15 @@
 class PicturesController < ApplicationController
 
   def index
-    @picture = Picture.order("Random()").first
+    @picture = Picture.random
   end
 
   def show
     @picture = Picture.find(params[:id])
+  end
+
+  def show_all
+    @pictures = Picture.all
   end
 
   def new
@@ -22,16 +26,23 @@ class PicturesController < ApplicationController
   end
 
   def edit
-
+    @picture = Picture.find(params[:id])
   end
 
   def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to pictures_path(:id)
+    else
+      render :update
+    end
   end
 
   def destroy
     @picture = Picture.find(params[:id])
     @picture.destroy
-    redirect_to picture_url
+    redirect_to pictures_url
   end
 
   private
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,7 @@
 class Picture < ActiveRecord::Base
+
+	scope :random, -> { order("Random()").first }
+	# scope :most_recent, -> { newest_first.first }
+	# scope :created_before, ->(time) { where("created_at < ?", time) }
+
 end
